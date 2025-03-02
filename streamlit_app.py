f4dfe49008e133ac78bfc43bab00814317c2ac3f
Gruvington <84064600+Gruvington@users.noreply.github.com>
2025-03-02 07:13:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
+from make_trial import make_trial
 
 # Create a selectbox for navigation
 tabs = st.selectbox("Select a tab", ["Make Trial", "Make Analysis Datasets", "Review"])
 
 # Show different content for each tab
 if tabs == "Make Trial":
-    st.write("This is content for Tab 1.")
+    make_trial(st)
 elif tabs == "Make Analysis Datasets":
     st.write("This is content for Tab 2.")
 elif tabs == "Review":

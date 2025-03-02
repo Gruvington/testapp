dc10da6dedafd0c740749e002eba32bd7be4205e
Gruvington <84064600+Gruvington@users.noreply.github.com>
2025-03-02 06:55:29 +0000

    tab test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Create a selectbox for navigation
+tabs = st.selectbox("Select a tab", ["Tab 1", "Tab 2", "Tab 3"])
+
+# Show different content for each tab
+if tabs == "Tab 1":
+    st.write("This is content for Tab 1.")
+    elif tabs == "Tab 2":
+        st.write("This is content for Tab 2.")
+        elif tabs == "Tab 3":
+            st.write("This is content for Tab 3.")

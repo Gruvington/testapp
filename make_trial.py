186ac0a45b7ccd9f6c6af7203af6baec22184af9
Gruvington <84064600+Gruvington@users.noreply.github.com>
2025-03-02 07:26:09 +0000

    Update make_trial.py

--- make_trial.py
+++ make_trial.py
@@ -1,15 +1,12 @@
 import pandas as pd
 
 def make_trial(st):
-   st.write("making a trial")
-
-
-
 
    # Create a sample dataframe
    data = {
     'Name': ['Vitacress 1', 'Vitacress 2', 'Flavourfresh 1', 'Flavourfresh 2'],
     'Start Date': ['2024-01-01', '2024-02-17', '2025-03-01', '2025-03-02'],
+'End Date': ['2024-01-04', '2024-02-23', '2025-03-03', '2025-03-09'],
     'Location': ['Aldergrove', 'Aldergrove', 'Bristop', 'Bristol']
 }
 

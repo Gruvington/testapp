5a32ce403a0f076744a7a3847921fc3336713d41
Gruvington <84064600+Gruvington@users.noreply.github.com>
2025-03-02 07:11:41 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 # Create a selectbox for navigation
-tabs = st.selectbox("Select a tab", ["Tab 1", "Tab 2", "Tab 3"])
+tabs = st.selectbox("Select a tab", ["Make Trial", "Make Analysis Datasets", "Review"])
 
 # Show different content for each tab
-if tabs == "Tab 1":
+if tabs == "Make Trial":
     st.write("This is content for Tab 1.")
-elif tabs == "Tab 2":
+elif tabs == "Make Analysis Datasets":
     st.write("This is content for Tab 2.")
-elif tabs == "Tab 3":
+elif tabs == "Review":
     st.write("This is content for Tab 3.")

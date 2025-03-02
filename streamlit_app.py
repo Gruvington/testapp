dddba669e1343eab8677dded1fc3f85b5db62f52
Gruvington <84064600+Gruvington@users.noreply.github.com>
2025-03-02 06:56:39 +0000

    tab fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ tabs = st.selectbox("Select a tab", ["Tab 1", "Tab 2", "Tab 3"])
 # Show different content for each tab
 if tabs == "Tab 1":
     st.write("This is content for Tab 1.")
-    elif tabs == "Tab 2":
-        st.write("This is content for Tab 2.")
-        elif tabs == "Tab 3":
-            st.write("This is content for Tab 3.")
+elif tabs == "Tab 2":
+    st.write("This is content for Tab 2.")
+elif tabs == "Tab 3":
+    st.write("This is content for Tab 3.")
